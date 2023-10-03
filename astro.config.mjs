1a7409778248605662246200b21710edb1ac4df6
Javier André Hernández Hernández <andrehdz@MacBook-Pro-de-Javier-2.local>
2023-10-03 15:27:12 -0600

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://clinicarenteria.com',
-  integrations: [mdx(), sitemap(), 
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
-    })]
+  integrations: [mdx(), sitemap(), tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false
+  }), partytown()]
 });
